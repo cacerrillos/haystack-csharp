848e14741a925e1af92e6687d8c3d7a3c58952c1
Charlie Cerrillos <ccerrillos@climatec.com>
2020-07-20 15:25:27 -0700

    iHServer

---
+++ ProjectHaystack/Server/HServer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectHaystack.Server {
+  public class HServer : HProj, IHServer {
+    HDateTime bootTime = HDateTime.now();
+
+    public override HDict about() {
+      return new HDictBuilder()
+        .add("haystackVersion", "3.0")
+        .add("serverTime", HDateTime.now())
+        .add("serverBootTime", bootTime)
+        .add("tz", HTimeZone.Default.ToString())
+        .toDict();
+        //.add(onAbout())
+    }
+
+    public HGrid formats() {
+      throw new NotImplementedException();
+    }
+
+    public override HGrid hisRead(HRef id, object range) {
+      throw new NotImplementedException();
+    }
+
+    public override void hisWrite(HRef id, HHisItem[] items) {
+      throw new NotImplementedException();
+    }
+
+    public HGrid invokeAction(HRef id, string action, HDict args) {
+      throw new NotImplementedException();
+    }
+
+    public HGrid invokeAction(HRef id, string action, HDict args, string mimetype) {
+      throw new NotImplementedException();
+    }
+
+    public HGrid ops() {
+      throw new NotImplementedException();
+    }
+
+    public HGrid pointWrite(HRef id, int level, string who, HVal val, HNum dur) {
+      throw new NotImplementedException();
+    }
+
+    public HGrid pointWriteArray(HRef id) {
+      throw new NotImplementedException();
+    }
+
+    public override HWatch watch(string id, bool bChecked) {
+      throw new NotImplementedException();
+    }
+
+    public override HWatch[] watches() {
+      throw new NotImplementedException();
+    }
+
+    public override HWatch watchOpen(string dis, HNum lease) {
+      throw new NotImplementedException();
+    }
+
+    protected override HGrid onReadAll(string filter, int limit) {
+      throw new NotImplementedException();
+    }
+
+    protected override HDict onReadById(HRef id) {
+      throw new NotImplementedException();
+    }
+
+    protected override HGrid onReadByIds(HRef[] ids) {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ ProjectHaystack/Server/IHServer.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectHaystack.Server {
+  public interface IHServer
+  {
+    HDict about();
+    
+    //HGrid call(string op, HGrid req);
+    //HGrid call(string op, HGrid req, string mimeType);
+    
+    //HGrid eval(string expr);
+    //HGrid[] evalAll(HGrid req, bool bChecked);
+    //HGrid[] evalAll(string[] exprs);
+    //HGrid[] evalAll(string[] exprs, bool bChecked);
+    HGrid formats();
+    HGrid hisRead(HRef id, object range);
+    void hisWrite(HRef id, HHisItem[] items);
+
+    HGrid invokeAction(HRef id, string action, HDict args);
+    HGrid invokeAction(HRef id, string action, HDict args, string mimetype);
+
+    HGrid ops();
+
+    HGrid pointWrite(HRef id, int level, string who, HVal val, HNum dur);
+    HGrid pointWriteArray(HRef id);
+  }
+}
